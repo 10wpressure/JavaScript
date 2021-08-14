6cc17bb3689eca5c0b96cea6caa8baded03e8712
Boris Kuznetsov <66515502+L0wPressu7e@users.noreply.github.com>
2021-08-14 15:41:59 +0300

    Add files via upload

---
+++ javascript-basic-projects/javascript-basic-projects/16-ES6-slider/setup/app.js
@@ -0,0 +1,67 @@
+import people from "./src/data.js";
+import get from "./src/getElement.js";
+
+const container = get(".slide-container");
+const nextBtn = get(".next-btn");
+const prevBtn = get(".prev-btn");
+
+// set slides
+container.innerHTML = people
+  .map((person, slideIndex) => {
+    const { img, name, job, text } = person;
+    let position = "next";
+
+    if (slideIndex === 0) {
+      position = "active";
+    }
+    if (slideIndex === people.length - 1) {
+      position = "last";
+    }
+    return `<article class="slide ${position}">
+            <img src=${img} class="img" alt="${name}" />
+            <h4>${name}</h4>
+            <p class="title">${job}</p>
+            <p class="text">${text}</p>
+            <div class="quote-icon">
+              <i class="fas fa-quote-right"></i>
+            </div>
+          </article>`;
+  })
+  .join("");
+
+const startSlider = (type) => {
+  const active = document.querySelector(".active");
+  const last = document.querySelector(".last");
+  let next = active.nextElementSibling;
+  if (!next) {
+    next = container.firstElementChild;
+  }
+  active.classList.remove("active");
+  last.classList.remove("last");
+  next.classList.remove("next");
+
+  if (type === "prev") {
+    active.classList.add("next");
+    last.classList.add("active");
+    next = last.previousElementSibling;
+    if (!next) {
+      next = container.lastElementChild;
+    }
+    next.classList.remove("next");
+
+    next.classList.add("last");
+    return;
+  }
+
+  active.classList.add("last");
+  last.classList.add("next");
+  next.classList.add("active");
+};
+
+nextBtn.addEventListener("click", () => {
+  startSlider("next");
+});
+
+prevBtn.addEventListener("click", () => {
+  startSlider("prev");
+});
